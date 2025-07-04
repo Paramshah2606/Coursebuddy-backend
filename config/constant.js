4feace4e68c57faf8c935a4df50882fcd04608a0
Paramshah2606 <155893648+Paramshah2606@users.noreply.github.com>
2025-07-04 14:51:53 +0530

    Update constant.js
    Configuration changes

--- config/constant.js
+++ config/constant.js
@@ -4,12 +4,10 @@ module.exports = {
     app_name:'Bagzag',
     app_url:'/v1/user',
     API_KEY: process.env.API_KEY,  
-    ENCRYPTION_KEY: 'cryptlib.getHashSha256("238a97b8c6cb73c22463e025bbefd49",32)' , 
-    ENCRYPTION_IV: "e025bbefd4941de2", 
     port_base_url:"http://localhost:8080/",
-    mailer_email:'paramshah2606@gmail.com',               
-    mailer_password:'giao hddk whhd agcc',
-    from_email:"paramshah2606@gmail.com",
+    mailer_email:process.env.mailer_email,               
+    mailer_password:process.env.mailer_password,
+    from_email:process.env.from_email,
     PORT:process.env.PORT,
     DB_Password:process.env.DB_Password,
     DB_Database:process.env.DB_Database,
@@ -26,7 +24,5 @@ module.exports = {
     Base_url_local:process.env.Base_url_local
   };
 
-// API_KEY="e28192c7502b7bbb04e981cc6d53939e"
-// Image_url=https://param/
 
   
